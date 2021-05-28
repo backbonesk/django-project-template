f74bf5380cf6e8b1199be98f39648779cf7f2ab0
Adam Žúrek <adamzurek14@gmail.com>
2021-05-28 11:58:15 +0200

    🥳 Deleted auth module.

--- {{cookiecutter.directory_name}}/apps/api/auth/decorators.py
+++
@@ -1,9 +0,0 @@
-from functools import wraps
-
-
-def signature_exempt(view_func):
-    """Mark a view function as being exempt from signature and apikey check."""
-    def wrapped_view(*args, **kwargs):
-        return view_func(*args, **kwargs)
-    wrapped_view.signature_exempt = True
-    return wraps(view_func)(wrapped_view)
