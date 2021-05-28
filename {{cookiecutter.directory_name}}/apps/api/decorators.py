60adefe5a53020cc500ff20ee214f11c52898539
Adam Žúrek <adamzurek14@gmail.com>
2021-05-28 11:57:22 +0200

    🥳 Moved signature_exempt decorator.

--- {{cookiecutter.directory_name}}/apps/api/decorators.py
+++ {{cookiecutter.directory_name}}/apps/api/decorators.py
@@ -11,3 +11,13 @@ def require_apikey(view_func):
         return view_func(*args, **kwargs)
     wrapped_view.require_apikey = True
     return wraps(view_func)(wrapped_view)
+
+
+def signature_exempt(view_func):
+    """
+    Mark a view function as being exempt from signature and apikey check.
+    """
+    def wrapped_view(*args, **kwargs):
+        return view_func(*args, **kwargs)
+    wrapped_view.signature_exempt = True
+    return wraps(view_func)(wrapped_view)
